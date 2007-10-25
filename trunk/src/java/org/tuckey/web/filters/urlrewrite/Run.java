d4cf3b2e504a7a63712bbdfce5ba55735afc2c4e
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2007-10-25 04:04:07 +0000

    extracted type method

--- trunk/src/java/org/tuckey/web/filters/urlrewrite/Run.java
+++ trunk/src/java/org/tuckey/web/filters/urlrewrite/Run.java
@@ -38,6 +38,7 @@ import org.tuckey.web.filters.urlrewrite.extend.RewriteMatch;
 import org.tuckey.web.filters.urlrewrite.utils.Log;
 import org.tuckey.web.filters.urlrewrite.utils.StringMatchingMatcher;
 import org.tuckey.web.filters.urlrewrite.utils.StringUtils;
+import org.tuckey.web.filters.urlrewrite.utils.TypeUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -205,36 +206,7 @@ public class Run {
      */
     private Class parseClass(String param) {
         // do shortcuts
-        Class paramClass = null;
-        if ("boolean".equals(param) || "bool".equals(param) || "z".equalsIgnoreCase(param)) paramClass = boolean.class;
-        if ("byte".equals(param) || "b".equalsIgnoreCase(param)) paramClass = byte.class;
-        if ("char".equals(param) || "c".equalsIgnoreCase(param)) paramClass = char.class;
-        if ("short".equals(param) || "s".equalsIgnoreCase(param)) paramClass = short.class;
-        if ("int".equals(param) || "i".equalsIgnoreCase(param)) paramClass = int.class;
-        if ("long".equals(param) || "l".equalsIgnoreCase(param)) paramClass = long.class;
-        if ("float".equals(param) || "f".equalsIgnoreCase(param)) paramClass = float.class;
-        if ("double".equals(param) || "d".equalsIgnoreCase(param)) paramClass = double.class;
-
-        if ("Boolean".equals(param) || "Bool".equals(param)) paramClass = Boolean.class;
-        if ("Byte".equals(param)) paramClass = Byte.class;
-        if ("Character".equalsIgnoreCase(param) || "C".equals(param)) paramClass = Character.class;
-        if ("Short".equals(param)) paramClass = Short.class;
-        if ("Integer".equals(param)) paramClass = Integer.class;
-        if ("Long".equals(param)) paramClass = Long.class;
-        if ("Float".equals(param)) paramClass = Float.class;
-        if ("Double".equals(param)) paramClass = Double.class;
-
-        if ("Class".equalsIgnoreCase(param)) paramClass = Class.class;
-        if ("Number".equalsIgnoreCase(param)) paramClass = Number.class;
-        if ("Object".equalsIgnoreCase(param)) paramClass = Object.class;
-        if ("String".equalsIgnoreCase(param) || "str".equalsIgnoreCase(param)) paramClass = String.class;
-
-        if ("HttpServletRequest".equalsIgnoreCase(param) || "req".equalsIgnoreCase(param) || "request".equalsIgnoreCase(param))
-            paramClass = HttpServletRequest.class;
-        if ("HttpServletResponse".equalsIgnoreCase(param) || "res".equalsIgnoreCase(param) || "response".equalsIgnoreCase(param))
-            paramClass = HttpServletResponse.class;
-        if ("ServletRequest".equalsIgnoreCase(param)) paramClass = ServletRequest.class;
-        if ("ServletResponse".equalsIgnoreCase(param)) paramClass = ServletResponse.class;
+        Class paramClass = TypeUtils.findClass(param);
 
         if ("javax.servlet.FilterChain".equalsIgnoreCase(param) || "FilterChain".equalsIgnoreCase(param)
                 || "chain".equalsIgnoreCase(param)) {
@@ -418,7 +390,7 @@ public class Run {
                 if (runMethodParamName != null) {
                     log.debug("need parameter from request called " + runMethodParamName);
                     Object matchObj = httpServletRequest.getParameter(runMethodParamName);
-                    param = getConvertedParam(runMethodParam, matchObj);
+                    param = TypeUtils.getConvertedParam(runMethodParam, matchObj);
 
                 } else if (runMethodParam.isAssignableFrom(HttpServletRequest.class)) {
                     param = httpServletRequest;
@@ -434,7 +406,7 @@ public class Run {
                     if (matchObjs != null && matchObjs.length > paramMatchCounter) {
                         matchObj = matchObjs[paramMatchCounter];
                     }
-                    param = getConvertedParam(runMethodParam, matchObj);
+                    param = TypeUtils.getConvertedParam(runMethodParam, matchObj);
                     paramMatchCounter++;
                 }
 
@@ -460,53 +432,6 @@ public class Run {
         return returned;
     }
 
-    private Object getConvertedParam(Class runMethodParam, Object matchObj) {
-        // see http://jakarta.apache.org/commons/beanutils/api/org/apache/commons/beanutils/MethodUtils.html
-        // for how to handle methods better
-        Object param = null;
-        if (matchObj == null) {
-            if (runMethodParam.isPrimitive()) {
-                if (runMethodParam.equals(boolean.class)) param = Boolean.FALSE;
-                else if (runMethodParam.equals(char.class)) param = new Character('\u0000');
-                else if (runMethodParam.equals(byte.class)) param = new Byte((byte) 0);
-                else if (runMethodParam.equals(short.class)) param = new Short((short) 0);
-                else if (runMethodParam.equals(int.class)) param = new Integer(0);
-                else if (runMethodParam.equals(long.class)) param = new Long(0L);
-                else if (runMethodParam.equals(float.class)) param = new Float(0f);
-                else if (runMethodParam.equals(double.class)) param = new Double(0d);
-            }
-        } else {
-            if (runMethodParam.equals(Boolean.class) || runMethodParam.equals(boolean.class))
-                param = Boolean.valueOf((String) matchObj);
-            else if (runMethodParam.equals(Character.class) || runMethodParam.equals(char.class))
-                param = new Character(((String) matchObj).charAt(0));
-            else if (runMethodParam.equals(Byte.class) || runMethodParam.equals(byte.class))
-                param = Byte.valueOf((String) matchObj);
-            else if (runMethodParam.equals(Short.class) || runMethodParam.equals(short.class))
-                param = Short.valueOf((String) matchObj);
-            else if (runMethodParam.equals(Integer.class) || runMethodParam.equals(int.class))
-                param = Integer.valueOf((String) matchObj);
-            else if (runMethodParam.equals(Long.class) || runMethodParam.equals(long.class))
-                param = Long.valueOf((String) matchObj);
-            else if (runMethodParam.equals(Float.class) || runMethodParam.equals(float.class))
-                param = Float.valueOf((String) matchObj);
-            else if (runMethodParam.equals(Double.class) || runMethodParam.equals(double.class))
-                param = Double.valueOf((String) matchObj);
-            else if (matchObj != null &&
-                    matchObj instanceof Throwable &&
-                    runMethodParam.isAssignableFrom(matchObj.getClass()))
-                param = matchObj;
-            else {
-                try {
-                    // last attempt
-                    param = runMethodParam.cast(matchObj);
-                } catch (ClassCastException e) {
-                    // do nothing
-                }
-            }
-        }
-        return param;
-    }
 
     /**
      * Run the underlying destroy methodStr on the run classStr.
