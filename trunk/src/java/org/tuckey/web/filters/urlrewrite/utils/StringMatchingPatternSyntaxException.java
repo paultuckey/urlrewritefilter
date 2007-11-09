f0fcf757fbb891f4cf1b80f932e1c0e5bb03b0d8
mdillon.i.am <mdillon.i.am@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2007-11-09 03:24:55 +0000

    Add serialVersionUID to another Serializable class

--- trunk/src/java/org/tuckey/web/filters/urlrewrite/utils/StringMatchingPatternSyntaxException.java
+++ trunk/src/java/org/tuckey/web/filters/urlrewrite/utils/StringMatchingPatternSyntaxException.java
@@ -41,6 +41,7 @@ import java.util.regex.PatternSyntaxException;
  * 
  */
 public class StringMatchingPatternSyntaxException extends Exception {
+    private static final long serialVersionUID = 4616654570576723975L;
 
     public StringMatchingPatternSyntaxException(PatternSyntaxException e) {
         super(e);
