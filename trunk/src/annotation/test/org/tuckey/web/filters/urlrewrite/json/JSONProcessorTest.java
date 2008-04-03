28465d2d0282c0ab3b7abc040b82fc9fe487871a
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2008-04-03 03:51:49 +0000

    json work on hold for a while

--- trunk/src/annotation/test/org/tuckey/web/filters/urlrewrite/json/JSONProcessorTest.java
+++ trunk/src/annotation/test/org/tuckey/web/filters/urlrewrite/json/JSONProcessorTest.java
@@ -7,7 +7,9 @@ import org.tuckey.web.filters.urlrewrite.utils.Log;
 
 import java.io.IOException;
 
-
+/**
+ * todo: future: json work on hold for now
+ */
 public class JSONProcessorTest extends TestCase {
 
 
@@ -23,7 +25,23 @@ public class JSONProcessorTest extends TestCase {
                 "    \"params\"  : { \"a\" : 12, \"b\" : 34, \"c\" : 56 }\n" +
                 "}");
         processor.service(request, response);
-        assertEquals("{\"result\":{\"class\":\"class org.tuckey.web.filters.urlrewrite.annotation.SampleMatchAction$ClientBean\"},\"version\":\"1.1\"}", response.getOutputStreamAsString());
+        assertEquals("{\"result\":{\"javaClass\":\"org.tuckey.web.filters.urlrewrite.annotation.SampleMatchAction$ClientBean\"},\"version\":\"1.1\"}", response.getOutputStreamAsString());
+    }
+
+
+    public void testEcho() throws IOException {
+        Log.setLevel("SYSOUT:DEBUG");
+        JSONProcessor processor = new JSONProcessor();
+        processor.setBase("org.tuckey.web.filters");
+        MockRequest request = new MockRequest("urlrewrite/annotation/SampleMatchAction");
+        MockResponse response = new MockResponse();
+        request.setBody("{\n" +
+                "    \"version\" : \"1.1\",\n" +
+                "    \"method\"  : \"echo\",\n" +
+                "    \"params\":[[1, 2, 3]]\n" +
+                "}");
+        processor.service(request, response);
+        assertEquals("{\"result\":[\"1\",\"2\",\"3\"],\"id\":5,\"version\":\"1.1\"}", response.getOutputStreamAsString());
     }
 
 
