7a3fe0570066bf4bd49005a7b18627b61ff41220
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2008-08-13 03:38:30 +0000

    removed cactus filter test dependency, container test now more accurately done with plain http client and cagro

--- trunk/src/test/org/tuckey/web/filters/urlrewrite/container/UrlRewriteFilterHttpTest.java
+++ trunk/src/test/org/tuckey/web/filters/urlrewrite/container/UrlRewriteFilterHttpTest.java
@@ -10,24 +10,20 @@ import org.xml.sax.SAXException;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 
 public class UrlRewriteFilterHttpTest extends TestCase {
 
     private String CONTENT = "<p>some content</p>";
 
-    private String baseUrl = "http://127.0.0.1:8080/";
+    private String baseUrl = "http://127.0.0.1:8080/webapp";
     HttpClient client = new HttpClient();
 
     protected void setUp() throws Exception {
-        Thread.currentThread().wait(200000);
         String systemPropBaseUrl = System.getProperty("test.base.url");
         if (systemPropBaseUrl != null) {
             baseUrl = systemPropBaseUrl;
-
-            //InetAddress addr = InetAddress.getLocalHost();
-            //System.out.println("hostname = " + addr.getHostName());
-            //baseUrl = baseUrl.replaceAll("localhost", addr.getHostName());
         }
     }
 
@@ -40,8 +36,9 @@ public class UrlRewriteFilterHttpTest extends TestCase {
 
     public void testSimpleDistEx() throws ServletException, IOException, SAXException {
         GetMethod method = new GetMethod(baseUrl + "/test/status/");
+        method.setFollowRedirects(false);
         client.executeMethod(method);
-        assertEquals("/blah/rewrite-status", method.getResponseHeader("Location").getValue());
+        assertEquals(baseUrl + "/rewrite-status", method.getResponseHeader("Location").getValue());
     }
 
     public void testBasicSets() throws ServletException, IOException, SAXException {
@@ -72,31 +69,40 @@ public class UrlRewriteFilterHttpTest extends TestCase {
 
     public void testTestAxis() throws ServletException, IOException {
         GetMethod method = new GetMethod(baseUrl + "/services/blah?qwerty");
+        method.setFollowRedirects(false);
         client.executeMethod(method);
-        assertEquals("/axis/services/blah?qwerty", method.getResponseHeader("Location").getValue());
+        assertEquals(baseUrl + "/axis/services/blah", method.getResponseHeader("Location").getValue());
     }
-
 
     public void testTestErik() throws ServletException, IOException {
         GetMethod method = new GetMethod(baseUrl + "/eriktest/hi.ho");
+        method.setFollowRedirects(false);
         method.addRequestHeader(new Header("host", "blah.com"));
         client.executeMethod(method);
         assertEquals("http://www.example.com/context/hi.ho", method.getResponseHeader("Location").getValue());
     }
 
+    /**
+     * todo: fix!!!
+     *
+     * @throws ServletException
+     * @throws IOException
+     */
     public void testTestEncode() throws ServletException, IOException {
-        GetMethod method = new GetMethod(baseUrl + "/went%20to%20bahamas/");
+        GetMethod method = new GetMethod(baseUrl + "/went%20to%20bahamas/;jsessionid=12243");
+        method.setFollowRedirects(false);
         client.executeMethod(method);
-        assertEquals("/bahamas/;jsess", method.getResponseHeader("Location").getValue());
+        assertEquals(baseUrl + "/bahamas/;jsess", method.getResponseHeader("Location").getValue());
     }
 
     /**
      * note, had trouble keeping true utf (multi byte) chars as cvs buggers them up!
      */
     public void testTestUtf() throws ServletException, IOException {
-        GetMethod method = new GetMethod(baseUrl + "/utf/Fêtel'haïvolapük/");
+        GetMethod method = new GetMethod(baseUrl + "/utf/" + URLEncoder.encode("Fêtel'haïvolapük", "UTF8") + "/");
+        method.setFollowRedirects(false);
         client.executeMethod(method);
-        assertEquals("/utf-redir/Fêtel'haïvolapük/", method.getResponseHeader("Location").getValue());
+        assertEquals(baseUrl + "/utf-redir/" + URLEncoder.encode("Fêtel'haïvolapük", "UTF8") + "/", method.getResponseHeader("Location").getValue());
     }
 
     public void testSimpleRun() throws ServletException, IOException {
