ffb2c05e81e6790589b7ecf44d9c950ae283349a
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2008-08-28 11:14:43 +0000

    change to be just a jquery plugin for now to limit scope

--- src/js/jquery.urlrewrite.js
+++ src/js/jquery.urlrewrite.js
@@ -1,81 +1,65 @@
+/*
+ * jquery plugin for easier handling of "hijax" style url loading.
+ *
+ */
 
+jQuery.ifhash = function(re, funcToInvoke) {
+    if ( typeof jQuery.urlrewriteData == "undefined" ) {
+        jQuery.urlrewriteData = {
+            urls: new Array(),
+            functions: new Array(),
+            urlId: 0,
+            listener: function(newLocation) {
+                //log("listener");
+                var matches = new Array();
+                var matched = $.grep(jQuery.urlrewriteData.urls, function(re, i) {
+                    //log("match #" + newLocation + " on " + re);
+                    matches[i] = re.exec("#" + newLocation);
+                    //log("res " + (matches[i] != null));
+                    return (matches[i] != null);
+                });
+                //log("got " + matched.length);
+                $.each(matched, function(i) {
+                    //log("on " + i);
+                    var func = jQuery.urlrewriteData.functions[i];
+                    var match = matches[i];
+                    match.shift();
+                    //log("calling " + func + " match " + match.length);
+                    func.apply(this, match);
+                })
+            }
+        }
 
-
-function UrlRewrite() {
-}
-
-// todo: make local
-var urlrewriteData = {
-    urls: new Array(),
-    functions: new Array(),
-    urlId: 0
-}
-
-
-UrlRewrite.prototype = {
-    add: function(fromRe, runFunction) {
-        var id =  urlrewriteData.urlId++;
-        log("add " + id);
-        urlrewriteData.urls[id] = fromRe;
-        urlrewriteData.functions[id] = runFunction;
-    },
-
-    listener: function(newLocation, historyData) {
-        log("listener");
-        var matches = new Array();
-        var matched = $.grep(urlrewriteData.urls, function(re, i) {
-            log("match #" + newLocation + " on " + re);
-            matches[i] = re.exec("#" + newLocation);
-            log("res " + (matches[i] != null));
-            return (matches[i] != null);
-        });
-        log("got " + matched.length);
-        $.each(matched, function(i) {
-            log("on " + i);
-            var func = urlrewriteData.functions[i];
-            var match = matches[i];
-            match.shift();
-            log("calling " + func + " match " + match.length);
-//            var groups = new Array();
-//            for ( var j = 0; j < func.length; j++) {
-//                groups[j] = match[j+1];
-            //}
-            log("about to call with params " + matches[i]);
-            func.apply(this, matches[i]);
-        })
-    },
-
-    init: function() {
-        log("init");
+        //log("init");
 
         // todo: window.dhtmlHistory.create();
         dhtmlHistory.initialize();
-        dhtmlHistory.addListener(this.listener);
-
+        dhtmlHistory.addListener(jQuery.urlrewriteData.listener);
     }
+    var id =  jQuery.urlrewriteData.urlId++;
+    //log("add " + id);
+    jQuery.urlrewriteData.urls[id] = re;
+    jQuery.urlrewriteData.functions[id] = funcToInvoke;
+    //log("done add");
+};
+
+jQuery.fn.ifhash = function(re, funcToInvoke) {
+    jqContext = this;
+    //log("shifted context");
+    jQuery.ifhash.apply(this, [re, function() {
+            //log("with context " + arguments + " " + jqContext);
+            funcToInvoke.apply(jqContext, arguments);
+        }]);
 }
+
+// usage example w/o jquery
+/*todo: do automatically instantiate our history object*/
+window.dhtmlHistory.create();
+
+
+
 
 function log(msg) {
     $("#log").append("<br/>"+msg);
 }
 
-
-
-// jquery integration
-
-jQuery.ifhash = function(re, funcToInvoke) {
-    if ( typeof jQuery.urlrewrite == "undefined" ) {
-        jQuery.urlrewrite = new UrlRewrite()
-        jQuery.urlrewrite.init();
-    }
-    jQuery.urlrewrite.add(re, funcToInvoke);
-    log("done add");
-};
-
-jQuery.fn.ifhash = function(re, funcToInvoke) {
-    log("shifted context");
-    jQuery.ifhash.apply(this, [re, function() {
-            log("with context " + arguments + " " + funcToInvoke);
-            funcToInvoke.apply(this, arguments);
-        }]);
-}
