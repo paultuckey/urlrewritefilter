b510558a0481b1ab40b6854a66e42601c547b566
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2008-09-15 22:57:52 +0000

    updated test

--- trunk/src/test/org/tuckey/web/filters/urlrewrite/container/WebappModStyleHttpTest.java
+++ trunk/src/test/org/tuckey/web/filters/urlrewrite/container/WebappModStyleHttpTest.java
@@ -1,3 +1,37 @@
+/**
+ * Copyright (c) 2005-2007, Paul Tuckey
+ * All rights reserved.
+ * ====================================================================
+ * Licensed under the BSD License. Text as follows.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   - Neither the name tuckey.org nor the names of its contributors
+ *     may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * ====================================================================
+ */
 package org.tuckey.web.filters.urlrewrite.container;
 
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -13,6 +47,14 @@ import java.io.IOException;
  */
 public class WebappModStyleHttpTest extends ContainerTestBase {
 
+    protected String getApp() {
+        return "webapp-mod-style";
+    }
+
+    public void testStatusRecord() throws IOException {
+        super.recordRewriteStatus();
+    }
+
     public void testSimpleTest() throws ServletException, IOException, SAXException {
         GetMethod method = new GetMethod(getBaseUrl() + "/simple/test");
         method.setFollowRedirects(false);
@@ -27,10 +69,5 @@ public class WebappModStyleHttpTest extends ContainerTestBase {
         assertTrue(method.getResponseBodyAsString().contains("Running Status"));
         assertFalse(method.getResponseBodyAsString().contains("Error"));
     }
-
-    protected String getBaseUrl() {
-        return super.getBaseUrl() + "/webapp-mod-style";
-    }
-
 
 }
