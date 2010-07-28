86a123a720c5f7abdd2ad3b12ffbfef7076293b3
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2010-07-28 23:50:51 +0000

    separated off performance related tests so they can be excluded from ant task

--- trunk/src/test/org/tuckey/web/filters/urlrewrite/UrlRewriterTest.java
+++ trunk/src/test/org/tuckey/web/filters/urlrewrite/UrlRewriterTest.java
@@ -37,7 +37,6 @@ package org.tuckey.web.filters.urlrewrite;
 import junit.framework.TestCase;
 import org.tuckey.web.testhelper.MockRequest;
 import org.tuckey.web.testhelper.MockResponse;
-import org.tuckey.web.testhelper.BenchmarkRunner;
 import org.tuckey.web.testhelper.MockFilterChain;
 import org.tuckey.web.testhelper.MockServletContext;
 import org.tuckey.web.filters.urlrewrite.test.MockRewriteMatch;
@@ -232,51 +231,6 @@ public class UrlRewriterTest extends TestCase {
         RewrittenUrl rewrittenRequest = urlRewriter.processRequest(request, response);
         assertNull(rewrittenRequest);
     }
-
-    /**
-     * Goal is to be able to process 2000 reqs a second for a simple rule set of 10000.
-     * Performance is obviously very CPU dependant, so we establish a benchmark for the machine the
-     * test is running on then base performance on that. This is a horribly rough way of determining
-     * performance, but it is good enough for this simple test case. 
-     */
-    public void testLoadsOfRules() throws IOException, ServletException, InvocationTargetException {
-        // turn off logging
-        Log.setLevel("ERROR");
-
-        // benchmark this machine to see what it can do...
-        float bench = new BenchmarkRunner().establishBenchmark();
-        float timePerRule = bench * (float) 0.0003; // ms per rule... 0.03% of the benchmark
-        System.out.println("using " + timePerRule + "ms per rule as the standard");
-
-        float testAmount = 10000; // number of times to run test
-
-        // test with 1000 rules, more than anybody would normally have
-        Conf conf = new Conf();
-        for (int i = 0; i < 1000; i++) {
-            NormalRule rule = new NormalRule();
-            rule.setFrom("^/([a-z]+)/([0-9]+)/" + i + "/$");
-            rule.setTo("/blah/a/$2/");
-            conf.addRule(rule);
-        }
-        conf.initialise();
-        UrlRewriter urlRewriter = new UrlRewriter(conf);
-
-        MockRequest request = new MockRequest("/dir/999/45/");
-        // warm up
-        urlRewriter.processRequest(request, response);
-
-        long timeStart = System.currentTimeMillis();
-        for (float i = 0; i < testAmount; i++) {
-            urlRewriter.processRequest(request, response);
-            if (i % 500 == 0 && i > 0) {
-                System.out.println("avg so far " + ((System.currentTimeMillis() - timeStart) / i) + "ms per rule");
-            }
-        }
-        long took = System.currentTimeMillis() - timeStart;
-        System.out.println("took " + took + "ms " + (took / testAmount) + "ms  per rule");
-        assertTrue("should take less than " + timePerRule + "ms per rule", (took / testAmount) < timePerRule);
-    }
-
 
     /**
      * Special test for rule application with no to element.
