1ea80a466cfffca7ec901ceb61da552cbc58f112
tuckey <tuckey@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2010-07-28 23:50:51 +0000

    separated off performance related tests so they can be excluded from ant task

--- src/test/org/tuckey/web/filters/urlrewrite/UrlRewriteWrappedResponseTest.java
+++ src/test/org/tuckey/web/filters/urlrewrite/UrlRewriteWrappedResponseTest.java
@@ -68,44 +68,5 @@ public class UrlRewriteWrappedResponseTest extends TestCase {
 
     }
 
-
-    /**
-     * Goal is to be able to process 1000 urls a second for a simple rule set of 1000.
-     */
-    public void testLoadsOfOutboundRules() {
-        // turn off logging
-        Log.setLevel("ERROR");
-
-        float testAmount = 10000; // number of times to run test
-        float timePerRule = 3;  // ms per rule
-
-        // test with 1000 rules
-        Conf conf = new Conf();
-        for (int i = 0; i < 1000; i++) {
-            OutboundRule rule = new OutboundRule();
-            rule.setFrom("^/([a-z]+)/([0-9]+)/" + i + "/$");
-            rule.setTo("/blah/a/$2/");
-            conf.addOutboundRule(rule);
-        }
-        conf.initialise();
-        UrlRewriter urlRewriter = new UrlRewriter(conf);
-
-        MockRequest request = new MockRequest("/dir/999/45/");
-        // warm up
-        UrlRewriteWrappedResponse urlRewriteWrappedResponse = new UrlRewriteWrappedResponse(response, request, urlRewriter);
-        urlRewriteWrappedResponse.encodeURL("/aaa");
-
-        long timeStart = System.currentTimeMillis();
-        for (float i = 0; i < testAmount; i++) {
-            urlRewriteWrappedResponse.encodeURL("/sdasd/asdasd/asdasd");
-            if (i % 500 == 0 && i > 0) {
-                System.out.println("avg so far " + ((System.currentTimeMillis() - timeStart) / i) + "ms per rule");
-            }
-        }
-        long took = System.currentTimeMillis() - timeStart;
-        System.out.println("took " + took + "ms " + (took / testAmount) + "ms per rule");
-        assertTrue("should take less than " + timePerRule + "ms per rule", (took / testAmount) < timePerRule);
-    }
-
 }
 
