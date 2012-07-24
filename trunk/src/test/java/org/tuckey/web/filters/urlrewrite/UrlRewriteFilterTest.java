3f71e785e909ac710a1392b52c4c6ff73949ff87
paul@tuckey.org <paul@tuckey.org@58d9aabf-6a2e-0410-9cc2-a579b616d322>
2012-07-24 09:55:25 +0000

    updated version test to support snapshot

--- trunk/src/test/java/org/tuckey/web/filters/urlrewrite/UrlRewriteFilterTest.java
+++ trunk/src/test/java/org/tuckey/web/filters/urlrewrite/UrlRewriteFilterTest.java
@@ -68,7 +68,7 @@ public class UrlRewriteFilterTest extends TestCase {
     public void testVersion() throws ServletException {
         String ver = UrlRewriteFilter.getFullVersionString();
         System.out.println(ver);
-        assertTrue("Ver bad " + ver, ver.matches("[0-9]+\\.[0-9]+\\.[0-9]+ build [0-9]+"));
+        assertTrue("Ver bad " + ver, ver.matches("[0-9]+\\.[0-9]+\\.[0-9]+(-SNAPSHOT)? build [0-9]+"));
     }
 
     public void testInitContext() throws ServletException {
