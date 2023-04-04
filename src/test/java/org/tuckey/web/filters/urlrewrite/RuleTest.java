9b35a66fc2c4a069008e1f61c65b4c3385982099
Brad Wood <brad@bradwood.com>
2023-04-04 11:59:43 -0400

    isFile handles encoded URIs

--- src/test/java/org/tuckey/web/filters/urlrewrite/RuleTest.java
+++ src/test/java/org/tuckey/web/filters/urlrewrite/RuleTest.java
@@ -820,6 +820,19 @@ public class RuleTest extends TestCase {
         assertEquals("/found-the-file", rewrittenUrl.getTarget());
     }
 
+    public void testEncodedFilename() throws InvocationTargetException, IOException, ServletException {
+        NormalRule rule = new NormalRule();
+        rule.setTo("/found-the-file");
+        Condition condition = new Condition();
+        condition.setType("request-filename");
+        condition.setOperator("isfile");
+        rule.addCondition(condition);
+        rule.initialise(new MockServletContext());
+        MockRequest request = new MockRequest("/conf%2Dtest1.xml");
+        NormalRewrittenUrl rewrittenUrl = (NormalRewrittenUrl) rule.matches(request.getRequestURI(), request, response);
+        assertEquals("/found-the-file", rewrittenUrl.getTarget());
+    }
+
     public void testNotFilename() throws InvocationTargetException, IOException, ServletException {
         NormalRule rule = new NormalRule();
         rule.setTo("/not-found-the-file");
