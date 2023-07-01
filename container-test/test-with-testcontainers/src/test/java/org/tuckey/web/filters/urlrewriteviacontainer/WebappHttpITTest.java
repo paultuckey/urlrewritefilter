3c2a51825518dbd85e1909c95b48315c58b65e3e
Paul Tuckey <paul@tuckey.org>
2023-07-01 16:45:34 +1200

    progress

--- container-test/test-with-testcontainers/src/test/java/org/tuckey/web/filters/urlrewriteviacontainer/WebappHttpITTest.java
+++ container-test/test-with-testcontainers/src/test/java/org/tuckey/web/filters/urlrewriteviacontainer/WebappHttpITTest.java
@@ -8,15 +8,15 @@
  * modification, are permitted provided that the following conditions
  * are met:
  * <p>
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   - Neither the name tuckey.org nor the names of its contributors
- *     may be used to endorse or promote products derived from this
- *     software without specific prior written permission.
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ * with the distribution.
+ * - Neither the name tuckey.org nor the names of its contributors
+ * may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
  * <p>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -47,8 +47,8 @@ import java.io.InputStream;
 import java.util.zip.GZIPInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 /**
@@ -111,7 +111,7 @@ public class WebappHttpITTest extends ContainerTestBase {
         client.executeMethod(method);
         assertEquals(403, method.getStatusCode()); // "should have status set",
         String CONTENT = "<p>some content</p>";
-        assertFalse(CONTENT.equals(StringUtils.trim(method.getResponseBodyAsString()))); // "should not output above content"
+        assertNotEquals(CONTENT, StringUtils.trim(method.getResponseBodyAsString())); // "should not output above content"
     }
 
     @Test
